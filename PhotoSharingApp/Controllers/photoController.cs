1a96c1e5ab46faef34449fe3fa173ac34d42f25e
hcentlili <tlilihcen@gmail.com>
2018-05-12 10:57:07 +0200

    edit

--- PhotoSharingApp/Controllers/photoController.cs
+++ PhotoSharingApp/Controllers/photoController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PhotoSharingApp.model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -6,12 +7,13 @@ using System.Web.Mvc;
 
 namespace PhotoSharingApp.Controller
 {
-    public class photo : System.Web.Mvc.Controller
+    public class PhotoController : System.Web.Mvc.Controller
     {
         // GET: photo
         public ActionResult Index()
         {
-            return View();
+            var photo = new Photo();
+            return View(photo);
         }
     }
 }
--- PhotoSharingApp/model/Photo.cs
+++ PhotoSharingApp/model/Photo.cs
@@ -9,18 +9,18 @@ namespace PhotoSharingApp.model
 {
     public class Photo
     {
-        private int PhotoId {get; set; }
-        private String Title {get; set; }
+        public int PhotoId {get; set; }
+        public String Title {get; set; }
         [DisplayName("Picture")]
-        private byte[] PhotoFile {get; set; }
-        [DataType.(DataType.MultilineText)]
-        private String Description {get; set; }
-        [DataType.(DataType.DateTime)]
+        public byte[] PhotoFile {get; set; }
+        [DataType(DataType.MultilineText)]
+        public String Description {get; set; }
+        [DataType(DataType.DateTime)]
         [DisplayName("Created Date")]
         [DisplayFormat(DataFormatString ="{0:MM/dd/yy}",ApplyFormatInEditMode =true)]
-        private object CreatedDate { get; set; }
-        
-        private String UserName { get; set; }
+        public object CreatedDate { get; set; }
+
+        public String UserName { get; set; }
         public virtual ICollection<Comment> Comments { get; set; }
     }
 }
