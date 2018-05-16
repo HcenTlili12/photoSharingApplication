dcf553c1a60df5145d13950f03e0038eefa1e24c
hcentlili <tlilihcen@gmail.com>
2018-05-16 02:35:38 +0200

    add controllers and create action

--- PhotoSharingApp/Controllers/photoController.cs
+++ PhotoSharingApp/Controllers/photoController.cs
@@ -17,5 +17,36 @@ namespace PhotoSharingApp.Controller
             var photo = new Photo();
             return View(p);
         }
+        public ActionResult First()
+        {
+            PhotoSharingContext context = new PhotoSharingContext();
+            Photo firstPhoto = context.photo.ToList()[0];
+            if (firstPhoto != null)
+            {
+                return View("Details", firstPhoto);
+            }
+            else
+            {
+                return HttpNotFound();
+            }
+        }
+        public ActionResult Create()
+        {
+            Photo newPhoto = new Photo();
+            return View("Create", newPhoto);
+}
     }
+
+   
+    /* public class PhotoController : System.Web.Mvc.Controller
+     {
+
+         public ActionResult Index()
+         {
+             List<Photo> a = new List<Photo>();
+             PhotoSharingContext context = new PhotoSharingContext();
+             a = context.photo.ToList<Photo>();
+             return View("Index",a);
+         }
+     }  */
 }
