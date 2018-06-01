1a509587645485dc2e2ee64e829602e3967b234b
hcentlili <tlilihcen@gmail.com>
2018-06-01 07:20:52 +0200

    task2 : import , method test_Index_return_view et method test_photogallery_model_type

---
+++ PhotoSharingTests/PhotoControllerTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Web.Mvc;
+using PhotoSharingApp.model;
+using PhotoSharingApp.Controller;
+using PhotoSharingApp.Controllers;
+
+namespace PhotoSharingTests
+{
+    [TestClass]
+    public class PhotoControllerTests
+    {
+        [TestMethod]
+        public void Test_Index_Return_View()
+        {
+            var controller = new PhotoController();
+            var result = controller.Index() as ViewResult;
+            Assert.AreEqual("Index", result.ViewName);
+        }
+        [TestMethod]
+        public void Test_PhotoGallery_Model_Type()
+        {
+
+        }
+    }
