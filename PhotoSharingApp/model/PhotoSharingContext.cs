7357361987166a5fbefef111026f8cb2fbba239c
hcentlili <tlilihcen@gmail.com>
2018-06-01 08:21:54 +0200

    ajout de l'interface Iphotosharing context et implementation des méthodes dans photosharing context :tâche 3

--- PhotoSharingApp/model/PhotoSharingContext.cs
+++ PhotoSharingApp/model/PhotoSharingContext.cs
@@ -4,12 +4,52 @@ using System.Linq;
 using System.Web;
 using System.Data;
 using System.Data.Entity;
+using PhotoSharingApp.Model;
 
 namespace PhotoSharingApp.model
 {
-    public class PhotoSharingContext : System.Data.Entity.DbContext
+    public class PhotoSharingContext : System.Data.Entity.DbContext, IPhotoSharingContext
     {
         public DbSet<Photo> photo { get; set; }
         public DbSet<Comment> comment { get; set; }
+        IQueryable<Comment> IPhotoSharingContext.Comments
+       {
+            get { return Comments; }
+                    }
+        public DbSet<Comment> Comments { get; set; }
+
+
+       int IPhotoSharingContext.SaveChanges()
+      {
+         return SaveChanges();
+       }
+
+      T IPhotoSharingContext.Add<T>(T entity)
+       {
+           return Set<T>().Add(entity);
+       }
+
+
+        Photo IPhotoSharingContext.FindPhotoById(int ID)
+        {
+            return Set<Photo>().Find(ID);
+        }
+
+        Comment IPhotoSharingContext.FindCommentById(int ID)
+        {
+            return Set<Comment>().Find(ID);
+        }
+
+
+        T IPhotoSharingContext.Delete<T>(T entity)
+        {
+            return Set<T>().Remove(entity);
+        }
+
+
+        IQueryable<Photo> IPhotoSharingContext.Photos
+        {
+    get { return photo; }
+            }
     }
 }
